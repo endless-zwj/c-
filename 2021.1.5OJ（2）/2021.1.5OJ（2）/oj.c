7dcbdb34d852316742177a0935611ae6d3918b82
endless-zwj <2595001484@qq.com>
2021-01-05 23:59:21 +0800

    oj

---
+++ 2021.1.5OJ（2）/2021.1.5OJ（2）/oj.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+//Ñî»ÔÈý½Ç
+#include <stdio.h>
+int main()
+{
+	int i, j, n;
+	int a[30][30] = { 0 };
+	scanf("%d", &n);
+	for (i = 0; i<n; i++)
+		a[i][0] = 1;
+	for (i = 0; i<n; i++)
+		a[i][i] = 1;
+	for (i = 2; i<n; i++)
+	for (j = 1; j<n - 1; j++)
+		a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
+	for (i = 0; i<n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			printf("%5d", a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
