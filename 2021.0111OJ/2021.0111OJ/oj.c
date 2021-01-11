f6383ccb5bb7f58c00dac9c9ea50eaf7424b6ccf
endless-zwj <2595001484@qq.com>
2021-01-11 12:43:39 +0800

    OJ

---
+++ 2021.0111OJ/2021.0111OJ/oj.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+//OJ 1
+#include <stdio.h>
+int main()
+{
+	int a = 40, c = 212;
+	int value;
+	value = (-8 + 22)*a - 10 + c / 2;
+	printf("%d", value);
+	return 0;
+}
+
