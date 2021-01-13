6d207c2921891472551b7cd9f6a9d8908a1a29a3
endless-zwj <2595001484@qq.com>
2021-01-13 22:03:49 +0800

    OJ

---
+++ 20210113/20210113/oj.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int c, k, r, l;
+	scanf("%d%d%d%d", &c, &k, &r, &l);
+	printf("%d\n%g\n%d\n", c*k, r*r*3.14, l*l);
+	return 0;
+}
+
